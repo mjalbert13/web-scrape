6e7ba48ad278522bc74bd17686f061d7c0fb3bc9
mjalbert13 <mjalbert13@gmail.com>
2020-02-21 13:50:00 -0500

    Printing scraped house data

--- scrape.py
+++ scrape.py
@@ -6,4 +6,33 @@ r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPR
 c = r.content
 soup = BeautifulSoup(c,"html.parser")
 
-print(soup.prettify)
+divs = soup.find_all("div",{"class":"propertyRow"})
+
+price = divs[0].find("h4",{"class":"propPrice"}).text.replace("\n",'').replace(" ","")
+
+for item in divs:
+    print(item.find("h4",{"class":"propPrice"}).text.replace("\n",'').replace(" ",""))
+    print(item.find_all("span", {"class":"propAddressCollapse"})[0].text)
+    print(item.find_all("span", {"class":"propAddressCollapse"})[1].text)
+    try:
+        print(item.find("span",{"class":"infoBed"}).find("b").text)
+    except:
+        print(None)
+
+    try:
+        print(item.find("span",{"class":"infoSqFt"}).find("b").text)
+    except:
+        print(None)
+
+    try:
+        print(item.find("span",{"class":"infoValueFullBath"}).find("b").text)
+    except:
+        print(None)
+
+    try:
+        print(item.find("span",{"class":"infoValueHalfBath"}).find("b").text)
+    except:
+        print(None)
+    
+    print("------------------")
+    
